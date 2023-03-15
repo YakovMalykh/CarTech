6f7c12ad39e8741d95e4dac3cba0d85c58aba4e0
YakovMalykh <to-yakoff@mail.ru>
2023-03-15 21:41:27 +0300

    initial commit

---
+++ src/main/java/ru/car/server/ServerApplication.java
@@ -0,0 +1,14 @@
+package ru.car.server;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
